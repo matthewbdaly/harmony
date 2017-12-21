08102e1b1e88091b83e87cee3abe64197b36a5eb
Matthew Daly <matthewbdaly@gmail.com>
2017-12-21 15:38:45 +0000

    Some progress adding the view

--- src/Http/Traits/Index.php
+++ src/Http/Traits/Index.php
@@ -6,10 +6,20 @@ use League\Fractal\Resource\Collection;
 
 trait Index
 {
-    public function index()
+    protected function getIndexData()
     {
         $resource = new Collection($this->getQueryset(), new $this->transformer);
-        $data = $this->fractal->createData($resource)->toArray();
-        return response()->json($data);
+        return $this->fractal->createData($resource)->toArray();
+    }
+
+    public function index()
+    {
+        $request = request();
+        $data = $this->getIndexData();
+        if ($request->ajax()) {
+            return response()->json($data);
+        } else {
+            return view('harmony::base', ['data' => $data]);
+        }
     }
 }
---
+++ src/Providers/HarmonyServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Matthewbdaly\Harmony\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Service provider
+ */
+class HarmonyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../views', 'harmony');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+		//
+    }
+}
