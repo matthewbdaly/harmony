e80b95366dc248f925db81c560cc96622c26750c
Matthew Daly <matthewbdaly@gmail.com>
2018-04-09 21:20:50 +0100

    Added the traits to the dummy controller

--- tests/Unit/Http/Controllers/DummyController.php
+++ tests/Unit/Http/Controllers/DummyController.php
@@ -3,7 +3,9 @@
 namespace Tests\Unit\Http\Controllers;
 
 use Matthewbdaly\Harmony\Http\Controllers\RestfulController;
+use Matthewbdaly\Harmony\Http\Traits\{Delete, Index, Show, Store, Update};
 
 class DummyController extends RestfulController
 {
+    use Delete, Index, Show, Store, Update;
 }
