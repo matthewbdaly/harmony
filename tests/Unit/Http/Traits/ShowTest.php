7d508a682b40bc0f35771d7328cf290096d8a4dc
Matthew Daly <matthewbdaly@gmail.com>
2018-04-09 21:45:26 +0100

    Added outline tests for remaining traits

--- tests/Unit/Http/Traits/ShowTest.php
+++ tests/Unit/Http/Traits/ShowTest.php
@@ -6,11 +6,11 @@ use Tests\TestCase;
 use Mockery as m;
 use Tests\Unit\Http\Controllers\DummyController;
 
-class StoreTest extends TestCase
+class ShowTest extends TestCase
 {
     public function testHasTrait()
     {
         $controller = m::mock('Tests\Unit\Http\Controllers\DummyController');
-        $this->assertParentHasTrait('Matthewbdaly\Harmony\Http\Traits\Store', $controller);
+        $this->assertParentHasTrait('Matthewbdaly\Harmony\Http\Traits\Show', $controller);
     }
 }
