82d13ca8a590ed310238a3200bff54fc69b5b6f0
Matthew Daly <matthewbdaly@gmail.com>
2018-04-24 22:09:33 +0100

    Amended delete trait to accept requests

--- src/Http/Traits/Delete.php
+++ src/Http/Traits/Delete.php
@@ -2,18 +2,33 @@
 
 namespace Matthewbdaly\Harmony\Http\Traits;
 
+use League\Fractal\Resource\Item;
+use Illuminate\Http\Request;
+
 trait Delete
 {
     /**
+     * Get deleted data response
+     *
+     * @return League\Fractal\Scope
+     */
+    protected function getDeletedData()
+    {
+        $resource = new Item([], $this->transformer);
+        return $this->fractal->createData($resource);
+    }
+
+    /**
      * Delete an object by ID
      *
+     * @param Request $request The request object.
      * @param mixed $id The ID of the object to delete.
      * @return Illuminate\Http\Response
      */
-    public function delete($id)
+    public function delete(Request $request, $id)
     {
         $item = $this->getQuerysetById($id);
         $item->delete();
-        return json([], 204);
+        return $this->renderResponse($this->getDeletedData(), 204);
     }
 }
