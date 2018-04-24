79d23bc84e489a05eeb5dea7fcde0603d7b89d54
Matthew Daly <matthewbdaly@gmail.com>
2018-04-24 23:29:23 +0100

    Added test for index trait

--- tests/Unit/Http/Traits/IndexTest.php
+++ tests/Unit/Http/Traits/IndexTest.php
@@ -13,4 +13,14 @@ class IndexTest extends TestCase
         $controller = m::mock('Tests\Unit\Http\Controllers\DummyController');
         $this->assertParentHasTrait('Matthewbdaly\Harmony\Http\Traits\Index', $controller);
     }
+
+    public function testIndex()
+    {
+        $fractal = $this->app->make('League\Fractal\Manager');
+        $repo = m::mock('Matthewbdaly\LaravelRepositories\Repositories\Interfaces\AbstractRepositoryInterface');
+        $repo->shouldReceive('all')->once()->andReturn([]);
+        $controller = new DummyController($fractal, $repo);
+        $response = $controller->index(); 
+        $this->assertEquals(200, $response->getStatusCode());
+    }
 }
