51732609c63891bf7412227f25d902ba22c9be40
Matthew Daly <matthewbdaly@gmail.com>
2018-04-24 23:31:17 +0100

    Added test for show method

--- tests/Unit/Http/Traits/ShowTest.php
+++ tests/Unit/Http/Traits/ShowTest.php
@@ -13,4 +13,16 @@ class ShowTest extends TestCase
         $controller = m::mock('Tests\Unit\Http\Controllers\DummyController');
         $this->assertParentHasTrait('Matthewbdaly\Harmony\Http\Traits\Show', $controller);
     }
+
+    public function testShow()
+    {
+        $fractal = $this->app->make('League\Fractal\Manager');
+        $repo = m::mock('Matthewbdaly\LaravelRepositories\Repositories\Interfaces\AbstractRepositoryInterface');
+        $model = m::mock('Illuminate\Database\Eloquent\Model');
+        $model->shouldReceive('getAttribute')->with('id')->once()->andReturn(1);
+        $repo->shouldReceive('findOrFail')->with(1)->once()->andReturn($model);
+        $controller = new DummyController($fractal, $repo);
+        $response = $controller->show(1); 
+        $this->assertEquals(200, $response->getStatusCode());
+    }
 }
